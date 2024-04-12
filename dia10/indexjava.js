fa04be9e2386a37fa3d1e68a8440693d5f5f62ea
Matheus Henrique Dos Santos <141289527+tteuwDev@users.noreply.github.com>
2024-04-12 15:56:44 -0300

    Update indexjava.js

--- dia10/indexjava.js
+++ dia10/indexjava.js
@@ -5,5 +5,8 @@ let contadorIndice = 0
 const numeroProcurado = ParSeInt(prompt("Digite o Numero a ser Procurado:"))
 
 for (let i = 0; i < 10; i++) {
-    const elemento = parseInt(prompt('Digite o elemento ${i+1}\:');)
+    const elemento = parseInt(prompt('Digite o elemento ${i+1}\:'));
+    array[i] = elemento
+
 }
+
